a7eb346f923ccafafd07bac3911fb19f941a705b
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-09-18 14:02:24 -0500

    METODO CREAR SERIE, LLAMANDO DATA CON URL SERVICE, CLASE LOGICA SIMPLE, FALTAN ALERTAS, BOTON ENRUTADO DEL FORMULARIO CANCELAR HACIA LISTA

--- src/app/serie-form/serie-form.component.ts
+++ src/app/serie-form/serie-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SerieService } from '../serie/serie.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-serie-form',
@@ -8,16 +10,41 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SerieFormComponent {
   form:FormGroup = this.fb.group({
-    titulo:['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]]
+    nombreSerie:['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]]
   });
 
+
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private serieService:SerieService,
+    private router:Router
   ){}
 
+
+  controlHasError(control:string, error:string): boolean{
+    return this.form.controls[control].hasError(error);
+  };
+  
+
   save(){
-    console.log('form valid', this.form.valid);
-    console.log('form', this.form.value);
+    //console.log('form valid', this.form.valid);
+    //console.log('form', this.form.value);
+    
+    if(this.form.invalid){
+      console.log("formulario invalido");
+      return;
+    }
+
+    let serie = this.form.value;
+    
+    this.serieService.create(serie)
+    .subscribe(()=>{
+      alert("Serie creada con exito");
+      this.router.navigate(['/']);
+    }, error =>{
+      console.error('Error al crear la serie', error);
+    });
+    
   }
 
 }
--- src/app/serie/serie.service.ts
+++ src/app/serie/serie.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { SeriePage } from '../interfaces/serie.interfaces';
+import { Serie, SeriePage } from '../interfaces/serie.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +14,10 @@ export class SerieService {
   paginate(){
     return this.http.get<SeriePage>('http://localhost:8080/api/series');
   }
+
+
+  create(serie: string){
+    return this.http.post<string>('http://localhost:8080/api/series/buscar',serie );
+  }
+
 }
