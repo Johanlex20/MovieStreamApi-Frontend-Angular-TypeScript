ecb6c32114574a059aa72759e23a801742973ab8
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2025-11-09 20:24:49 -0500

    ajuste variables environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-   //apiBase: 'http://192.168.20.103:8080/api',
-   //apiBase:'https://apiforogood.modoblu.com/api',
+   apiBase: 'https://apimovie.modoblu.com',   // backend público
+   apiBaseFront: 'https://movie.modoblu.com'
 }
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
     production: false,
+    //apiBase: 'http://192.168.20.103:8082',  // backend local / contenedor
+    //apiBaseFront: 'http://192.168.20.103:4202' // frontend local / contenedor
     apiBase: 'http://localhost:8080/api',
     apiBaseFront: 'http://localhost:4200',
-   
 }
 
 
